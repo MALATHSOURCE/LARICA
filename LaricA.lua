eac155a7622f958b2c6fb05e0cdb9615380552ae
MALATHSOURCE <85354544+MALATHSOURCE@users.noreply.github.com>
2021-06-12 19:14:30 +0300

    Add files via upload

--- LaricA.lua
+++ LaricA.lua
@@ -4782,7 +4782,8 @@ send(msg.chat_id_,msg.id_,' ['..ta.title_..']('..link..')')
 else                
 local linkgpp = json:decode(https.request('https://api.telegram.org/bot'..token..'/exportChatInviteLink?chat_id='..msg.chat_id_))
 if linkgpp.ok == true then 
-linkgp = '🦇• LinK GrOup : ━═━═━═━═━\n ['..ta.title_..']('..linkgpp.result..')'
+linkgp = '🦇• LinK GrOup : 
+━═━═━═━═━\n ['..ta.title_..']('..linkgpp.result..')'
 else
 linkgp = '• لا يوجد رابط ارسل ضع رابط'
 end  
