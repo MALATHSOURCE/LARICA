1e92b615e19a86d2016ef46e1e29fd8eb915e128
MALATHSOURCE <85354544+MALATHSOURCE@users.noreply.github.com>
2021-06-18 04:13:32 +0300

    Add files via upload

--- LaricA.lua
+++ LaricA.lua
@@ -8737,7 +8737,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevLaricA(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/MALATHSOURCE/files_LaricA/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/MALATHSOURCE/Files_LaricA/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -8775,7 +8775,7 @@ t = "*• الملف ↺ {"..file.."}\n• تم تعطيله وحذفه بنجا
 else
 t = "*• بالتاكيد تم تعطيل وحذف ملف ↺ {"..file.."} \n✓*"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MALATHSOURCE/files_LaricA/master/files_LaricA/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/MALATHSOURCE/Files_LaricA/master/Files_LaricA/"..file)
 if res == 200 then
 os.execute("rm -fr LaricA_Files/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -8795,7 +8795,7 @@ t = "*• بالتاكيد تم تنزيل وتفعيل ملف ↺ {"..file.."} 
 else
 t = "*• الملف ↺ {"..file.."}\n• تم تنزيله وتفعيله بنجاح \n*"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MALATHSOURCE/files_LaricA/master/files_LaricA/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/MALATHSOURCE/Files_LaricA/master/Files_LaricA/"..file)
 if res == 200 then
 local chek = io.open("LaricA_Files/"..file,'w+')
 chek:write(json_file)
